85238c6a7bc0181ce72e9c0a2f6f72eb76919b0b
Neka Kerr <anneika.kerr@gmail.com>
2021-04-07 16:45:22 -0400

    broken code

--- src/Game.java
+++ src/Game.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
-    private ArrayList allPlayers = new ArrayList();
-    private Game ghost = new Game();
+    private ArrayList<User> allPlayers = new ArrayList();
+    private ArrayList<User> allActivePlayers = new ArrayList();
+    private Board ghost = new Board();
 
 // get all players to be created at once and
     // put them all in the allPlayers arraylist
@@ -17,11 +18,45 @@ public class Game {
             if (response != "y") {
                 User newPlayer = new User();
                 allPlayers.add(newPlayer);
+                allActivePlayers.add(newPlayer);
                 morePlayers = false;
             }
         }
+
+        ghost.setCurrentPlayer(allActivePlayers.get(0));
     }
 
+    public void playGhost() {
+        populateAllPlayers();
+
+        while (hasWon() == false) {
+            for (int i = 0; i <= allActivePlayers.size() - 1; i++) {
+                System.out.println(ghost.viewBoard());
+                User ele = allActivePlayers.get(i);
+                char letter = ele.nextGuess();
+                ghost.getWordSoFar().append(letter);
+
+                // get user input to see if any other players challenge
+                    // figure out who is challenging
+                    // check against dictionary
+                    // delete current player from the game and set current player to next
+                    // element
+                    // break into the next loop
+
+                if (allActivePlayers.get(i + 1) == null ) {
+                    continue;
+                }
+
+                ghost.setCurrentPlayer(allActivePlayers.get(i + 1));
+            }
+
+        }
+
+    }
+
+    public Boolean hasWon() {
+        return allPlayers.size() != 1;
+    }
 
     public String getUserInput() {
         Scanner scanner;
@@ -30,5 +65,11 @@ public class Game {
         String response = new String();
         response = scanner.nextLine();
         return response;
+    }
+
+    // for testing purposes
+    public static void main(String[] args) {
+        Game ghost = new Game();
+        ghost.playGhost();
     }
 }
